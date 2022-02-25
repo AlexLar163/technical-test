088d8551a6a86811af3b2f6344af0259c3d0d1f3
AlexLar163 <alexlar163@gmail.com>
2022-02-25 04:39:26 -0500

    Optimized file initial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,28 @@ import "../../styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Head from "next/head";
+import { ApolloProvider } from "@apollo/client";
+import client from "../utilities/apollo-client";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
+      <Head>
+        <title>Rick and Morty</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ApolloProvider client={client}>
+        <Header />
+        <div
+          className="
+          container 
+          mx-auto "
+        >
+          <Component {...pageProps} />
+        </div>
+        <Footer />
+      </ApolloProvider>
     </>
   );
 }
