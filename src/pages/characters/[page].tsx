5ea92c42da5d85a4aa00b4997b6074beba759706
AlexLar163 <alexlar163@gmail.com>
2022-02-25 04:38:09 -0500

    Added routing

---
+++ src/pages/characters/[page].tsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from "react";
+import { useQuery, useLazyQuery } from "@apollo/client";
+import getAllCharacters from "../../graphql/queries/getAllCharacters";
+import CardLoading from "../../skeleton/CardLoading";
+import Card from "../../components/Card";
+import Pagination from "../../components/Pagination";
+import { CharacterInterface } from "../../interfaces/characterInterface";
+
+export default function Characters(props: any) {
+  const page = parseInt(props.page) || 1;
+  const [getCharacters, results] = useLazyQuery(getAllCharacters);
+  const [characters, setCharacters] = useState([] as Array<CharacterInterface>);
+  // const [characters, setCharacters] = useState([] as Array<any>);
+
+  useEffect(() => {
+    getCharacters({ variables: { page: page } });
+    if (results.data) {
+      setCharacters(results.data.characters.results);
+    }
+  }, [results.data,page]);
+
+  return (
+    <>
+      <Pagination />
+      <div
+        className="
+      container
+      sm
+      grid 
+      grid-cols-1 
+      sm:grid-cols-2 
+      md:grid-cols-3 
+      lg:grid-cols-4 
+      xl:grid-cols-5 
+      gap-5"
+      >
+        {characters.length === 0 ? (
+          <>
+            <CardLoading />
+            <CardLoading />
+            <CardLoading />
+            <CardLoading />
+            <CardLoading />
+            <CardLoading />
+            <CardLoading />
+            <CardLoading />
+            <CardLoading />
+            <CardLoading />
+            <CardLoading />
+            <CardLoading />
+          </>
+        ) : (
+          characters.map((char: CharacterInterface) => <Card key={`${char.name}-${char.id}`} data={char} />)
+        )}
+      </div>
+      <Pagination />
+    </>
+  );
+}
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { page: "1" } }],
+    fallback: true,
+  };
+}
+export async function getStaticProps(context: any) {
+  const { params } = context;
+  return { props: { ...params } };
+}
